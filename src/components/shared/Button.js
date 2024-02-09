864594db889c42c3ef7dd73036a0c5204f3aa185
Юлия Рысева <iulia.ryseva@gmail.com>
2024-02-09 23:29:33 +0300

    add fonts, button and base modal

---
+++ src/components/shared/Button.js
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import { colors } from '../../constants/colors';
+
+export const BUTTON_TYPES = {
+    filled: 'filled',
+    outlined: 'outlined',
+};
+
+export const BUTTON_SIZE = {
+    md: 'md',
+    sm: 'sm',
+};
+
+const TYPE_TO_BACKGROUND = {
+    [BUTTON_TYPES.filled]: colors.gray,
+    [BUTTON_TYPES.outlined]: 'transparent',
+};
+
+const TYPE_TO_COLOR = {
+    [BUTTON_TYPES.filled]: 'white',
+    [BUTTON_TYPES.outlined]: colors.gray,
+};
+
+const ButtonStyled = styled.button`
+    --spacing: 10px;
+    --spacing_x2: calc(var(--spacing) * 2);
+    --spacing_x4: calc(var(--spacing) * 4);
+
+    outline: none;
+    border:  2px solid ${({background}) => background ?? colors.gray};
+    color: ${({$type}) => TYPE_TO_COLOR[$type]};
+    background-color: ${({background, $type}) => background ?? TYPE_TO_BACKGROUND[$type]};
+    padding: ${({$size}) => $size === BUTTON_SIZE.md ? 'var(--spacing_x2) var(--spacing_x4)' : 'var(--spacing) var(--spacing_x2)'};
+    border-radius: 45px;
+    font-size: 18px;
+`;
+
+export const Button = ({ type = 'filled', size = 'md', ...props }) => <ButtonStyled $type={type} $size={size} {...props} />;
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    gray: 'rgb(51, 51, 51)',
+    blue: 'rgb(0, 150, 255)',
+    violet: 'rgb(115, 37, 150)',
+    purple: 'rgb(175, 0, 255)',
+};
