fe8b224938c8afe688b9750e419de1d51a76326d
dmmanshin <ya.manshin@mail.ru>
2024-02-29 13:26:34 +0300

    fix saving

--- src/api/saveToMailList.js
+++ src/api/saveToMailList.js
@@ -1,8 +1,8 @@
-import {MAIL_LIST_ENDPOINT_URL, MAIL_LIST_API_KEY} from "../constants/api";
+import {MAIL_LIST_ENDPOINT_URL, MAIL_LIST_API_KEY, MAIL_LIST_ID} from "../constants/api";
 
 export function saveToMailList(data) {
     return fetch(
-        `${MAIL_LIST_ENDPOINT_URL}?format=json&api_key=${MAIL_LIST_API_KEY}&field_names[0]=id&field_names[1]=name&field_names[2]=email&data[0][0]=${data.id}&data[0][1]=${data.name}&data[0][2]=${data.email}`,
+        `${MAIL_LIST_ENDPOINT_URL}?format=json&api_key=${MAIL_LIST_API_KEY}&field_names[0]=id_vtb&field_names[1]=Name&field_names[2]=email&field_names[3]=email_list_ids&data[0][0]=${data.id}&data[0][1]=${data.name}&data[0][2]=${data.email}&data[0][3]=${MAIL_LIST_ID}`,
         { method: 'POST' },
     )
         .then(resp => resp.json())
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
 export const LEADERBOARD_BIN_ID = '65dcd16b266cfc3fde8fa4b1'
 export const LEADERBOARD_ENDPOINT_URL = `https://api.jsonbin.io/v3/b/${LEADERBOARD_BIN_ID}`
 
-// TODO Заменить API Key
-export const MAIL_LIST_API_KEY = '65dcd16b266cfc3fde8fa4b1'
+export const MAIL_LIST_ID = '2'
+export const MAIL_LIST_API_KEY = '6a173mswjuh4kpgc9f9s5ejckj8rf7ksgnoj59pe'
 export const MAIL_LIST_ENDPOINT_URL = 'https://api.unisender.com/ru/api/importContacts'
