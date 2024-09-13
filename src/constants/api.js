4bca25e9fdf7ae51317d1c31788b846e2f3c6931
dmmanshin <ya.manshin@mail.ru>
2024-09-13 15:20:12 +0300

    fix

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ import { FTClient } from "ft-client";
 export const LEADERBOARD_ENDPOINT_URL = "https://ft-admin-api.sjuksin.ru/";
 export const LEADERBOARD_PROJECT_ID = "vtb-tower";
 
-export const MAIL_LIST_ID = "2";
+export const MAIL_LIST_ID = "83";
 export const MAIL_LIST_API_KEY = "6a173mswjuh4kpgc9f9s5ejckj8rf7ksgnoj59pe";
 export const MAIL_LIST_ENDPOINT_URL = "https://api.unisender.com/ru/api/importContacts";
 
